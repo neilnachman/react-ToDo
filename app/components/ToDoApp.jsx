4b6a304b8707bcf6b29f1ea65a923d9a7c2c13c5
neil <neilnach@gmail.com>
2017-01-11 20:06:20 +0200

    todo app component

---
+++ app/components/ToDoApp.jsx
@@ -0,0 +1,12 @@
+var React = require("react");
+var ToDoApp = React.createClass({
+  render : function(){
+    return (
+      <div>
+        todoApp.jsx
+      </div>
+    );
+  }
+});
+
+module.exports = ToDoApp;
