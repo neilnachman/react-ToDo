ed7d08dda852e3b5e2ae8af9794161aa34bbb587
neil <neilnach@gmail.com>
2017-01-15 21:21:02 +0200

    adding todo reducer

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,5 @@
+var uuid = require("node-uuid");
+var moment = require("moment");
 export var searchTextReducer = (state = '',action)=>{
   switch(action.type){
     case 'SET_SEARCH_TEXT':
@@ -13,5 +15,34 @@ export var showCompletedReducer = (state = false , action)=>{
       return !state;
    default:
       return state;
+  };
+};
+
+export var todosReducer = (state = [] , action)=>{
+  switch(action.type){
+    case 'ADD_TODO':
+    return[
+      ...state,
+      {
+        id : uuid(),
+        text : action.text,
+        completed : false,
+        createdAt : moment().unix(),
+        completedAt : undefined
+      }
+    ];
+    case 'TOGGLE_TODO':
+    var updatedTodos = state.map((todo)=>{
+      if(todo.id === action.id){
+        return{
+          ...todo,
+          completed: !todo.completed,
+          completedAt:!todo.completed ? moment().unix() : undefined
+        };
+    };
+  });
+    return updatedTodos;
+    default:
+    return state;
   };
 };
